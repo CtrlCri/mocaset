2d15eae112e00aa76830e1f94e77c8bd89c50c85
armycrih <armycrih@gmail.com>
2022-10-16 20:29:18 -0300

    encode: type int Ok

--- DataOps-Challenge/main.py
+++ DataOps-Challenge/main.py
@@ -13,18 +13,25 @@ class BinaryParser():
         return self.size
 
     def decode(self, trama: bytearray, format: list):
-        pass
+        # Deserializar trama en Array
+        data = array("i")
+        data.frombytes(trama)
+        # Deserializar en Object
+        _object = {}
+
+
+        return data
 
     def encode(self, object: dict, format: list):
         # Serializar oject
         data = array("i")
-        for item in object:  # Me quedo sólo con los valores y los ingreso en un array
-            data.append( item[value] ) 
-        self.buffer = data.tobytes
+        for item in object.values():  # Me quedo sólo con los valores y los ingreso en un array
+            data.append(item) 
+        self.buffer = data.tobytes()
         # Obtener tamaño en bits
         count = 0
-        for t in format:
-            count += t["len"]
+        for f in format:
+            count += f["len"]
         self.size = count
     
 
@@ -36,8 +43,15 @@ if __name__ == "__main__":
         {"tag": "WaterLevel", "type": "int", "len": 8 }
     ]
     data_1 = { "PTemp": 268, "BattVolt.value": 224, "WaterLevel": 115 }
-
+    # Encode
     bp = BinaryParser()
     bp.encode(data_1, format_1)
-    print(hexlify(bp.get_buffer()))
+    data_encoded = bp.get_buffer()
+    print(hexlify(data_encoded))
     print(bp.get_size())
+    # Decode
+    data_decoded = bp.decode(data_encoded, format_1)
+    print(data_decoded)
+
+
+
